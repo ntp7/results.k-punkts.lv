62d5a48e8c052b602dfe1e7c2e58382bffd6008e
ntp7 <31138933+ntp7@users.noreply.github.com>
2025-08-23 22:32:15 +0200

    files

---
+++ firebaseconfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBMb0jBPIDPdKJhvw8EdTT8zDIjmh9n4Uk",
+    authDomain: "kpunkts-617f1.firebaseapp.com",
+    databaseURL: "https://kpunkts-617f1-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "kpunkts-617f1",
+    storageBucket: "kpunkts-617f1.appspot.com",
+    messagingSenderId: "402643964732",
+    appId: "1:402643964732:web:5d15d7317ace4554a94439",
+    measurementId: "G-F65ZJ05F38"
+  };
+
+export default firebaseConfig;
